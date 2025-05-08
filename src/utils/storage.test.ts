dc4309b114cb4e97333f73881db8199c700b15ba
nines <127805872+nines75@users.noreply.github.com>
2025-05-08 17:44:40 +0900

    storage.test.tsを更新

--- src/utils/storage.test.ts
+++ src/utils/storage.test.ts
@@ -1,7 +1,11 @@
 import { fakeBrowser } from "#imports";
 import { defaultSettings } from "@/utils/config.js";
 import {
+    customMerge,
     getAllData,
+    getLogData,
+    getSettingsData,
+    loadSettings,
     removeAllData,
     removeData,
     setLog,
@@ -9,57 +13,94 @@ import {
 } from "@/utils/storage.js";
 import { describe, expect, it, beforeEach } from "vitest";
 import { testLog } from "./data.js";
+import { stringify } from "superjson";
+import { Settings } from "@/types/storage/settings.types.js";
+import { PartialDeep } from "type-fest";
 
 describe("storage", () => {
     beforeEach(() => {
         fakeBrowser.reset();
     });
 
-    it("remove all data", async () => {
-        // set data
+    it("customMerge()", () => {
+        const oldObj = {
+            nest: {
+                a: true,
+            },
+            array: [1],
+            map: new Map([["a", 1]]),
+            set: new Set([1]),
+        };
+        const newObj = {
+            nest: {
+                b: false,
+            },
+            array: [2],
+            map: new Map([["b", 2]]),
+            set: new Set([2]),
+        };
+
+        expect(customMerge(oldObj, newObj)).toEqual({
+            nest: {
+                a: true,
+                b: false,
+            },
+            array: [2],
+            map: new Map([["b", 2]]),
+            set: new Set([2]),
+        });
+    });
+
+    it("loadSettings()", async () => {
+        const newSettings = {
+            isCommentFilterEnabled: false,
+        } satisfies PartialDeep<Settings>;
+
+        await setSettings(newSettings);
+
+        expect(await loadSettings()).toEqual({
+            ...defaultSettings,
+            ...newSettings,
+        });
+    });
+
+    it("removeAllData()", async () => {
         await setSettings(defaultSettings);
         await setLog(testLog, 1);
 
-        // remove
         await removeAllData();
 
         expect(await getAllData()).toEqual({});
     });
 
-    it("remove data", async () => {
-        // set data
+    it("removeData()", async () => {
         await setSettings(defaultSettings);
         await setLog(testLog, 1);
 
-        // remove
         await removeData(["log-1"]);
 
         expect(await getAllData()).toEqual({ settings: defaultSettings });
     });
 
-    // 仕様外の型を保存しているためうまくいかない
-    // it("get all data", async () => {
-    //     // set data
-    //     await setSettings(defaultSettings);
-    //     await setLog(testLog, 1);
+    it("getAllData()", async () => {
+        await setSettings(defaultSettings);
+        await setLog(testLog, 1);
 
-    //     expect(await getAllData()).toEqual({
-    //         settings: defaultSettings,
-    //         "log-1": testLog,
-    //     });
-    // });
+        expect(await getAllData()).toEqual({
+            settings: defaultSettings,
+            "log-1": stringify(testLog),
+        });
+    });
 
-    // it("get log data", async () => {
-    //     // set data
-    //     await setLog(testLog, 1);
+    it("getLogData()", async () => {
+        await setLog(testLog, 1);
 
-    //     expect(await getAllData()).toEqual({ "log-1": testLog });
-    // });
+        expect(await getLogData(1)).toEqual(testLog);
+    });
 
-    it("get settings data", async () => {
-        // set data
+    it("getSettingsData()", async () => {
         await setSettings(defaultSettings);
 
-        expect(await getAllData()).toEqual({ settings: defaultSettings });
+        expect(await getSettingsData()).toEqual(defaultSettings);
     });
 });
