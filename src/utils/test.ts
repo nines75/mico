87093bebaaef0b3bec273f247bc80abd273d4535
nines <127805872+nines75@users.noreply.github.com>
2025-05-18 14:32:05 +0900

    save-video-log.tsのテストを追加

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -1,83 +1,6 @@
 import { NiconicoComment, Thread } from "@/types/api/comment.types.js";
 import { LogData } from "@/types/storage/log.types.js";
 
-export const testLog = {
-    processingTime: { filtering: 51, fetchTag: 50, saveVideoLog: 5 },
-    videoData: {
-        count: {
-            items: {
-                easyComment: 0,
-                ngUserId: 0,
-                ngScore: 0,
-                ngCommand: 0,
-                ngWord: 2,
-            },
-            blocked: 2,
-            loaded: 2,
-            include: 0,
-            exclude: 0,
-            disable: 2,
-        },
-        log: {
-            ngUserId: new Map(),
-            ngScore: [],
-            ngCommand: new Map(),
-            ngWord: new Map([
-                [
-                    "/test/i",
-                    new Map([
-                        ["test", ["1000"]],
-                        ["test2", ["1001"]],
-                    ]),
-                ],
-            ]),
-            strictNgUserIds: new Set(),
-            noToUserId: new Map([
-                [1, "nvc:RpBQf40dpW85ue3CiT8UZ6AUer6"],
-                [3, "nvc:RpBQf40dpW85ue3CiT8UZ6AUer6"],
-            ]),
-            comments: new Map([
-                [
-                    "1000",
-                    {
-                        id: "1000",
-                        no: 1,
-                        vposMs: 0,
-                        body: "test",
-                        commands: ["big", "ue", "184"],
-                        isMyPost: false,
-                        isPremium: false,
-                        nicoruCount: 0,
-                        nicoruId: null,
-                        postedAt: "2025-05-07T15:00:00+09:00",
-                        score: 0,
-                        source: "trunk",
-                        userId: "nvc:RpBQf40dpW85ue3CiT8UZ6AUer6",
-                    },
-                ],
-                [
-                    "1001",
-                    {
-                        id: "1001",
-                        no: 3,
-                        vposMs: 2000,
-                        body: "test2",
-                        commands: ["big", "shita", "184"],
-                        isMyPost: false,
-                        isPremium: false,
-                        nicoruCount: 0,
-                        nicoruId: null,
-                        postedAt: "2025-05-07T15:00:01+09:00",
-                        score: 0,
-                        source: "trunk",
-                        userId: "nvc:RpBQf40dpW85ue3CiT8UZ6AUer6",
-                    },
-                ],
-            ]),
-        },
-    },
-} as const satisfies LogData;
-
 export const testThreads = (() => {
     const base = {
         id: "1000",
@@ -96,7 +19,9 @@ export const testThreads = (() => {
     } satisfies NiconicoComment;
     const forks = ["owner", "main", "easy"] as const;
 
-    const getComment = (comment: Partial<NiconicoComment>): NiconicoComment => {
+    const createComment = (
+        comment: Partial<NiconicoComment>,
+    ): NiconicoComment => {
         return {
             ...base,
             ...comment,
@@ -110,8 +35,8 @@ export const testThreads = (() => {
                     fork: fork,
                     commentCount: 2,
                     comments: [
-                        getComment({}),
-                        getComment({ id: "1001", no: 2 }),
+                        createComment({}),
+                        createComment({ id: "1001", no: 2 }),
                     ],
                 };
             case "main":
@@ -119,7 +44,7 @@ export const testThreads = (() => {
                     fork: fork,
                     commentCount: 3,
                     comments: [
-                        getComment({
+                        createComment({
                             id: "1002",
                             no: 3,
                             commands: ["big", "184"],
@@ -128,7 +53,7 @@ export const testThreads = (() => {
                             body: "テスト",
                             nicoruCount: 29,
                         }),
-                        getComment({
+                        createComment({
                             id: "1003",
                             no: 4,
                             commands: ["184", "device:Switch"],
@@ -137,7 +62,7 @@ export const testThreads = (() => {
                             body: "テストコメント",
                             nicoruCount: 30,
                         }),
-                        getComment({
+                        createComment({
                             id: "1004",
                             no: 5,
                             commands: ["big", "184", "device:Switch"],
@@ -153,16 +78,16 @@ export const testThreads = (() => {
                     fork: fork,
                     commentCount: 0,
                     comments: [
-                        getComment({
+                        createComment({
                             id: "1005",
                             no: 6,
-                            userId: "4QhgYaZbMAYEUOdwDQ7a8KeX96p",
+                            userId: "nvc:4QhgYaZbMAYEUOdwDQ7a8KeX96p",
                             body: "！？",
                         }),
-                        getComment({
+                        createComment({
                             id: "1006",
                             no: 7,
-                            userId: "4QhgYaZbMAYEUOdwDQ7a8KeX96p",
+                            userId: "nvc:4QhgYaZbMAYEUOdwDQ7a8KeX96p",
                             body: "うぽつ",
                         }),
                     ],
@@ -170,6 +95,52 @@ export const testThreads = (() => {
         }
     });
 })();
+
+export const testLog = {
+    processingTime: { filtering: 51, fetchTag: 50, saveVideoLog: 5 },
+    videoData: {
+        count: {
+            items: {
+                easyComment: 2,
+                ngUserId: 2,
+                ngScore: 1,
+                ngCommand: 1,
+                ngWord: 1,
+            },
+            blocked: 7,
+            loaded: 7,
+            include: 0,
+            exclude: 0,
+            disable: 0,
+        },
+        log: {
+            ngUserId: new Map([
+                ["nvc:RpBQf40dpW85ue3CiT8UZ6AUer6", ["1000", "1001"]],
+            ]),
+            ngScore: ["1002"],
+            ngCommand: new Map([["big", ["1004"]]]),
+            ngWord: new Map([
+                ["/コメント/i", new Map([["テストコメント", ["1003"]]])],
+            ]),
+            strictNgUserIds: new Set(),
+            noToUserId: new Map([
+                [1, "nvc:RpBQf40dpW85ue3CiT8UZ6AUer6"],
+                [2, "nvc:RpBQf40dpW85ue3CiT8UZ6AUer6"],
+                [3, "nvc:mkJLLB69n1Kx9ERDlwY23nS6xyk"],
+                [4, "nvc:vcG0xFnXKcGl81lWoedT3VOI3Qj"],
+                [5, "nvc:llNBacJJPE6wbyKKEioq3lO6515"],
+                [6, "nvc:4QhgYaZbMAYEUOdwDQ7a8KeX96p"],
+                [7, "nvc:4QhgYaZbMAYEUOdwDQ7a8KeX96p"],
+            ]),
+            comments: new Map(
+                getComments(["1000", "1001", "1002", "1004", "1003"]) as [
+                    string,
+                    NiconicoComment,
+                ][],
+            ),
+        },
+    },
+} as const satisfies LogData;
 
 export function hasComment(threads: Thread[], ids: string[]) {
     return threads.some((thread) =>
@@ -179,4 +150,19 @@ export function hasComment(threads: Thread[], ids: string[]) {
 
 export function replaceInclude(filter: string) {
     return filter.replace(/include/g, "exclude");
+}
+
+function getComments(ids: string[]) {
+    const comments: [string, NiconicoComment | undefined][] = ids.map((id) => [
+        id,
+        testThreads
+            .flatMap((thread) => thread.comments)
+            .find((comment) => comment.id === id),
+    ]);
+    comments.forEach(([_, comment]) => {
+        comment?.commands.forEach(
+            (command, i, commands) => (commands[i] = command.toLowerCase()),
+        );
+    });
+    return comments;
 }
