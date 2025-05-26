816d7f63675d6030a8e019f058c5005daed65532
nines <127805872+nines75@users.noreply.github.com>
2025-05-27 01:12:30 +0900

    refactor: 不要なuseStateを使わない

--- src/entrypoints/options/components/ui/FilterArea.tsx
+++ src/entrypoints/options/components/ui/FilterArea.tsx
@@ -1,7 +1,6 @@
 import Editor from "./Editor.js";
 import { useShallow } from "zustand/shallow";
 import { useStorageStore } from "@/utils/store.js";
-import { useState } from "react";
 import { commentFilterSettings } from "@/utils/config.js";
 
 export type FilterId = "ngUserId" | "ngCommand" | "ngWord";
@@ -12,9 +11,7 @@ export interface FilterAreaProps {
 }
 
 export default function FilterArea() {
-    const [id, setId] = useState<FilterId>(
-        useStorageStore.getState().settings.defaultFilter,
-    );
+    const id = useStorageStore((state) => state.settings.defaultFilter);
     const [text, save] = useStorageStore(
         useShallow((state) => [state.settings[id], state.saveSettings]),
     );
@@ -26,10 +23,7 @@ export default function FilterArea() {
                     <button
                         key={filter.id}
                         className={`filter-button${id === filter.id ? " selected-filter-button" : ""}`}
-                        onClick={() => {
-                            save({ defaultFilter: filter.id });
-                            setId(filter.id);
-                        }}
+                        onClick={() => save({ defaultFilter: filter.id })}
                     >
                         <span>{filter.name}</span>
                     </button>
