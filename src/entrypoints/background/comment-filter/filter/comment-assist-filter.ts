aed93cfa93d403cc459b2cc3da96b26adea43608
nines <127805872+nines75@users.noreply.github.com>
2026-03-02 21:50:28 +0900

    refactor: 定数をclassの外側で定義する

--- src/entrypoints/background/comment-filter/filter/comment-assist-filter.ts
+++ src/entrypoints/background/comment-filter/filter/comment-assist-filter.ts
@@ -1,7 +1,15 @@
+// -------------------------------------------------------------------------------------------
+// 実装方針: https://github.com/nines75/mico/issues/41
+// -------------------------------------------------------------------------------------------
+
 import type { Thread } from "@/types/api/comment.types";
 import { Filter } from "../filter";
 import type { CommonLog } from "@/types/storage/log.types";
 import { pushCommonLog } from "@/utils/util";
+
+// コメントアシスト機能のリリース日は2025/02/26
+// https://blog.nicovideo.jp/niconews/243601.html
+const RELEASE_DATE = new Date("2025-02-26T00:00:00+09:00");
 
 export class CommentAssistFilter extends Filter<CommonLog> {
     protected override log: CommonLog = new Map();
@@ -14,11 +22,10 @@ export class CommentAssistFilter extends Filter<CommonLog> {
 
             const { id, commands, postedAt, body } = comment;
 
-            const releaseDate = new Date("2025-02-26T00:00:00+09:00"); // https://blog.nicovideo.jp/niconews/243601.html
             const date = new Date(postedAt);
             if (Number.isNaN(date.getTime())) return true; // 有効なDateであるか確認
 
-            if (commands.length === 0 && date >= releaseDate) {
+            if (commands.length === 0 && date >= RELEASE_DATE) {
                 pushCommonLog(this.log, body, id);
                 this.filteredComments.set(id, comment);
                 this.blockedCount++;
