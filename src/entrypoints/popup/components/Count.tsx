e3e5bdc1b04ab2effca1e8850f37d020555081b5
nines <127805872+nines75@users.noreply.github.com>
2026-05-03 22:47:05 +0900

    refactor: ポップアップのカウント情報をグループごとに分けて表示する

--- src/entrypoints/popup/components/Count.tsx
+++ src/entrypoints/popup/components/Count.tsx
@@ -5,40 +5,56 @@ export default function Count() {
 
   return (
     <>
-      {[
-        {
-          name: "コメントのブロック数:",
-          value: createPercentage(count?.blockedComment, count?.loadedComment),
-        },
-        {
-          name: "動画のブロック数:",
-          value: createPercentage(count?.blockedVideo, count?.loadedVideo),
-        },
-        {
-          name: "有効化されたルールの数:",
-          value: count?.include,
-        },
-        {
-          name: "無効化されたルールの数:",
-          value: count?.exclude,
-        },
-        {
-          name: "無効化されたコマンドの数:",
-          value: count?.disable,
-        },
-      ].map(({ name, value }) => {
-        if (value === undefined || (typeof value === "number" && value <= 0))
-          return null;
+      <div className="info-container">
+        {[
+          {
+            name: "コメントのブロック数:",
+            value: createPercentage(
+              count?.blockedComment,
+              count?.loadedComment,
+            ),
+          },
+          {
+            name: "動画のブロック数:",
+            value: createPercentage(count?.blockedVideo, count?.loadedVideo),
+          },
+        ].map(({ name, value }) => (
+          <div className="info" key={name}>
+            {name}
+            <span className="info-value">{value}</span>
+          </div>
+        ))}
+      </div>
+      {(() => {
+        const config = [
+          {
+            name: "有効化されたルールの数:",
+            value: count?.include,
+          },
+          {
+            name: "無効化されたルールの数:",
+            value: count?.exclude,
+          },
+          {
+            name: "無効化されたコマンドの数:",
+            value: count?.disable,
+          },
+        ].filter(({ value }) => value !== undefined && value > 0);
+
+        // 不必要にcontainerをレンダリングしないようにする
+        if (config.length === 0) return null;
 
         return (
-          <section key={name}>
-            <span className="info">
-              {name}
-              <span className="info-value">{value}</span>
-            </span>
-          </section>
+          <div className="info-container">
+            {config.map(({ name, value }) => (
+              <div className="info" key={name}>
+                {name}
+                <span className="info-value">{value}</span>
+              </div>
+            ))}
+          </div>
         );
-      })}
+      })()}
     </>
   );
 }
