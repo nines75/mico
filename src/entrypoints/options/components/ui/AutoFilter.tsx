ecc06683ca41a9864c66868bae31ef3c3c47e557
nines <127805872+nines75@users.noreply.github.com>
2026-05-24 17:27:34 +0900

    feat: Autoフィルターのルールを検索する機能を追加

--- src/entrypoints/options/components/ui/AutoFilter.tsx
+++ src/entrypoints/options/components/ui/AutoFilter.tsx
@@ -5,18 +5,53 @@ import { X } from "lucide-react";
 import { useShallow } from "zustand/shallow";
 import { VList } from "virtua";
 import { escapeNewline } from "@/utils/util";
+import { useMemo, useState } from "react";
 
 export default function AutoFilter() {
+  const [query, setQuery] = useState("");
   const autoFilter = useSettingsStore((state) => state.settings.autoFilter);
 
-  return (
-    <VList className="rule-container">
-      {autoFilter.map((rule) => {
-        if (rule.id === undefined) return null;
+  const rules = useMemo(
+    () =>
+      autoFilter.filter((rule) => {
+        if (query === "") return true;
 
-        return <Rule rule={rule} key={rule.id} />;
-      })}
-    </VList>
+        return (
+          rule.pattern?.includes(query) === true ||
+          rule.source?.includes(query) === true ||
+          rule.context?.includes(query) === true ||
+          rule.memo?.includes(query) === true ||
+          rule.include?.videoIds?.flat().some((id) => id.includes(query)) ===
+            true ||
+          (rule.target !== undefined &&
+            Object.entries(rule.target).some(([key, value]) => {
+              if (!value) return false;
+
+              return decamelize(key, { separator: "-" }).includes(query);
+            }))
+        );
+      }),
+    [autoFilter, query],
+  );
+
+  return (
+    <>
+      <input
+        className="search"
+        placeholder="ルールを検索"
+        value={query}
+        onChange={(event) => {
+          setQuery(event.target.value);
+        }}
+      />
+      <VList className="rule-container">
+        {rules.map((rule) => {
+          if (rule.id === undefined) return null;
+
+          return <Rule rule={rule} key={rule.id} />;
+        })}
+      </VList>
+    </>
   );
 }
 
