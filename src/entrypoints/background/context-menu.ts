50ed921115f3a115c935a10adec2c8a4a20b8b3f
nines <127805872+nines75@users.noreply.github.com>
2026-08-07 04:47:37 +0900

    refactor: コンテキストメニュー用の処理を整理

--- src/entrypoints/background/context-menu.ts
+++ src/entrypoints/background/context-menu.ts
@@ -1,3 +1,4 @@
+import type { Log } from "@/types/storage/log.types";
 import { getActiveTab, notify } from "@/utils/browser";
 import { getLog } from "@/utils/db";
 import { getLogIdViaMessage } from "@/utils/messaging";
@@ -16,61 +17,66 @@ export async function addRuleFromUrl(url: string | undefined, memo?: string) {
   const logId = await getLogIdViaMessage(tab?.id);
   const log = logId === undefined ? undefined : await getLog(logId);
 
-  const videoId = url?.match(
-    /^https:\/\/www\.nicovideo\.jp\/(?:watch|shorts)\/([^?]+)/,
-  )?.[1];
-  if (videoId !== undefined) {
-    const videoTitle = log?.video?.allVideos.find(
-      (video) => video.id === videoId,
-    )?.title;
+  for (const getRule of [getVideoIdRule, getOwnerIdRule]) {
+    const result = getRule(url, log);
+    if (result === undefined) continue;
 
     await addAutoRule([
       {
-        pattern: videoId,
         source: "contextMenu",
-        target: { videoId: true },
-        ...(videoTitle !== undefined && {
-          context: `video-title: ${videoTitle}`,
-        }),
+        ...result.rule,
         ...(memo !== undefined && memo !== "" && { memo }),
       },
     ]);
 
     if (settings.notifyOnManualNg) {
-      const context = videoTitle === undefined ? "" : ` (${videoTitle})`;
-
-      await notify(`以下の動画IDをNG登録しました\n\n${videoId}${context}`);
-    }
-
-    return;
-  }
-
-  const ownerId = url?.match(
-    /^https:\/\/(?:www\.nicovideo\.jp\/user|ch\.nicovideo\.jp\/channel)\/([^?]+)/,
-  )?.[1];
-  if (ownerId !== undefined) {
-    const ownerName = log?.video?.allVideos.find(
-      (video) => video.owner.id === ownerId,
-    )?.owner.name;
-
-    await addAutoRule([
-      {
-        pattern: ownerId,
-        source: "contextMenu",
-        target: { videoOwnerId: true },
-        ...(isString(ownerName) && { context: `owner-name: ${ownerName}` }),
-        ...(memo !== undefined && memo !== "" && { memo }),
-      },
-    ]);
-
-    if (settings.notifyOnManualNg) {
-      const context = isString(ownerName) ? ` (${ownerName})` : "";
-
-      await notify(`以下のユーザーIDをNG登録しました\n\n${ownerId}${context}`);
+      await notify(result.message);
     }
 
     return;
   }
 
   await notify("NG登録に失敗しました");
+}
+
+function getVideoIdRule(url: string | undefined, log: Log | undefined) {
+  const videoId = url?.match(
+    /^https:\/\/www\.nicovideo\.jp\/(?:watch|shorts)\/([^?]+)/,
+  )?.[1];
+  if (videoId === undefined) return;
+
+  const videoTitle = log?.video?.allVideos.find(
+    (video) => video.id === videoId,
+  )?.title;
+
+  return {
+    rule: {
+      pattern: videoId,
+      target: { videoId: true },
+      ...(videoTitle !== undefined && {
+        context: `video-title: ${videoTitle}`,
+      }),
+    },
+    message: `以下の動画IDをNG登録しました\n\n${videoId}${videoTitle === undefined ? "" : ` (${videoTitle})`}`,
+  };
+}
+
+function getOwnerIdRule(url: string | undefined, log: Log | undefined) {
+  const ownerId = url?.match(
+    /^https:\/\/(?:www\.nicovideo\.jp\/user|ch\.nicovideo\.jp\/channel)\/([^?]+)/,
+  )?.[1];
+  if (ownerId === undefined) return;
+
+  const ownerName = log?.video?.allVideos.find(
+    (video) => video.owner.id === ownerId,
+  )?.owner.name;
+
+  return {
+    rule: {
+      pattern: ownerId,
+      target: { videoOwnerId: true },
+      ...(isString(ownerName) && { context: `owner-name: ${ownerName}` }),
+    },
+    message: `以下のユーザーIDをNG登録しました\n\n${ownerId}${isString(ownerName) ? ` (${ownerName})` : ""}`,
+  };
 }
